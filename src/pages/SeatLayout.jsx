170106bbff90f029968000b8ef378d41dfb5ca58
naqel09 <zeronime17@gmail.com>
2025-12-10 01:18:05 +0700

    make a seat layout

--- src/pages/SeatLayout.jsx
+++ src/pages/SeatLayout.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const SeatLayout = () => {
-  return (
-    <div>
+
+  const {id, date} = useParams()
+  const [selectedSeats, setSelectedSeats]= useState([])
+  const [selectedTime, setSelectedTime] = useState (null)
+  const [show,setShow] = useState(null)
+
+  const navigate = useNavigate()
+
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id === id)
+    if(show){
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+    }
+  }
+
+  useEffect(()=>{
+    getShow()
+  },[])
+
+  return show ? (
+    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
       
     </div>
+  ):(
+    <Loading/>
   )
 }
 
